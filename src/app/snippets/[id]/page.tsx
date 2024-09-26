465fb6111e8e31049934def6b32820fac897ebe9
blopez024 <bmlopez024@gmail.com>
2024-09-25 18:43:51 -0700

    styling the show page

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,10 +1,10 @@
-import { db } from "@/db"
-import { notFound } from "next/navigation"
+import { db } from '@/db';
+import { notFound } from 'next/navigation';
 
 interface SnippetShowPageProps {
     params: {
-        id: string
-    }
+        id: string;
+    };
 }
 
 export default async function SnippetShowPage(props: SnippetShowPageProps) {
@@ -12,7 +12,7 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
 
     const snippet = await db.snippet.findFirst({
         where: { id: parseInt(props.params.id) }
-    })
+    });
 
     if (!snippet) {
         return notFound();
@@ -20,7 +20,18 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
 
     return (
         <>
-            {snippet.title}
+            <div className="flex m-4 justify-between items-center">
+                <h1 className="text-xl font-bold">{snippet.title}</h1>
+                <div className='flex gap-4'>
+                    <button className="p-2 border rounded">Edit</button>
+                    <button className="p-2 border rounded">Delete </button>
+                </div>
+            </div>
+            <pre className='p-3 border rounded bg-gray-200 border-gray-200'>
+                <code>
+                    {snippet.code}
+                </code>
+            </pre>
         </>
-    )
+    );
 }
