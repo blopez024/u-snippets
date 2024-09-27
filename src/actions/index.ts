7d78a56382ce3f29d81e9bc980f75b11f4585724
blopez024 <bmlopez024@gmail.com>
2024-09-26 20:12:58 -0700

    calling a server action from a client component

---
+++ src/actions/index.ts
@@ -0,0 +1,14 @@
+'use server';
+
+import { db } from '@/db';
+import { redirect } from 'next/navigation';
+
+export async function editSnippet(id: number, code: string) {
+    await db.snippet.update({
+        where: { id },
+        data: { code },
+    });
+
+    redirect(`/snippets/${id}`)
+    // console.log(id, code);
+}
--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -1,31 +1,39 @@
-'use client'
+'use client';
 
-import type { Snippet } from "@prisma/client"
-import { Editor } from "@monaco-editor/react"
-import { useState } from "react"
+import type { Snippet } from '@prisma/client';
+import { Editor } from '@monaco-editor/react';
+import { useState } from 'react';
+import * as actions from '@/actions'
 
 interface SnippetEditFormProps {
-    snippet: Snippet
+    snippet: Snippet;
 }
 
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
     const [code, setCode] = useState(snippet.code);
 
-    const handleEditorChange = (value: string = "") => {
-        setCode(value)
-    }
+    const handleEditorChange = (value: string = '') => {
+        setCode(value);
+    };
+
+    const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code)
+
 
     return (
         <>
             <Editor
-                height='40vh'
+                height="40vh"
                 theme="vs-dark"
                 language="javascript"
                 defaultValue={snippet.code}
                 options={{ minimap: { enabled: false } }}
                 onChange={handleEditorChange}
             />
+            <form action={editSnippetAction}>
+                <button type='submit' className='p-2 border rounded'>
+                    Save
+                </button>
+            </form>
         </>
-    )
-
+    );
 }
